403635781f98d860a18e700bf07a3600f5e3a063
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-19 10:01:23 -0400

    solution

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
 
+  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target && array[j] !== array[i]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 /* 
-  Write the Big O time complexity of your function here
+Write the Big O time complexity of your function here
+0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Itirate over the array,
+  for each individual element, itirate over the array again to check the sum of values
+  take the sum of the initial element and the second element
+  compare if the sum of the elements is equal to the target value
+  return true if the values matchup
 */
 
 /*
   Add written explanation of your solution here
+  Itirates over an array, during each pass of an element it itirates over array again
+  during each pass of the second itirations elements, it adds the sum of the first element
+  and the second element, and compares it to the target. Returns true if it matches
+  Returns false if the entire array has been itirated over with no matches
 */
 
 // You can run `node index.js` to view these console logs
